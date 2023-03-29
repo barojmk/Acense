fc3c6ed21b98d6dda70c37a008e95bfb239284db
barojmk <jennymin06@gmail.com>
2023-03-30 00:36:58 +0900

    delete

--- Acense Camping Community/src/main/java/com/spring/biz/heart/HeartService.java
+++
@@ -1,9 +0,0 @@
-package com.spring.biz.heart;
-
-public interface HeartService {
-   public boolean insertHeart(HeartVO vo);
-   public boolean deleteHeart(HeartVO vo);
-   public HeartVO selectHeartCnt(HeartVO vo);
-   public boolean deleteHeartMember(HeartVO vo);
-   public boolean updateHeartMember(HeartVO vo);
-}
